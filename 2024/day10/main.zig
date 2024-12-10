0419f2326248907717f0321ec9b79c7ff81efaaa
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-12-11 00:24:43 +0100

    day 10 part 2

--- 2024/day10/main.zig
+++ 2024/day10/main.zig
@@ -27,40 +27,46 @@ const Loc = struct {
     c: usize,
 };
 
-pub fn countTrails(r: usize, c: usize, next_node: u8, map: Map, solutions: *std.AutoHashMap(Loc, void)) !void {
+pub fn countTrails(r: usize, c: usize, next_node: u8, map: Map, solutions: *std.AutoHashMap(Loc, void)) !usize {
     if (next_node >= 10) {
         try solutions.put(Loc{ .r = r, .c = c }, {});
+        return 1;
     }
 
+    var count: usize = 0;
     if (r > 0 and next_node == map.at(r - 1, c)) {
-        try countTrails(r - 1, c, next_node + 1, map, solutions);
+        count += try countTrails(r - 1, c, next_node + 1, map, solutions);
     }
     if (r < (map.height - 1) and next_node == map.at(r + 1, c)) {
-        try countTrails(r + 1, c, next_node + 1, map, solutions);
+        count += try countTrails(r + 1, c, next_node + 1, map, solutions);
     }
     if (c > 0 and next_node == map.at(r, c - 1)) {
-        try countTrails(r, c - 1, next_node + 1, map, solutions);
+        count += try countTrails(r, c - 1, next_node + 1, map, solutions);
     }
     if (c < (map.width - 1) and next_node == map.at(r, c + 1)) {
-        try countTrails(r, c + 1, next_node + 1, map, solutions);
+        count += try countTrails(r, c + 1, next_node + 1, map, solutions);
     }
+    return count;
 }
 
 pub fn part1(map: Map, allocator: std.mem.Allocator) !void {
-    var result: usize = 0;
+    var num_trails: usize = 0;
+    var score: usize = 0;
     for (0..map.height) |r| {
         for (0..map.width) |c| {
             if (map.at(r, c) == 0) {
                 var solutions = std.AutoHashMap(Loc, void).init(allocator);
                 defer solutions.deinit();
 
-                try countTrails(r, c, 1, map, &solutions);
-                std.log.debug("Found trailhead at {}, {}: score {}", .{ r, c, solutions.count() });
-                result += solutions.count();
+                const cur_num_trails = try countTrails(r, c, 1, map, &solutions);
+                std.log.debug("Found trailhead at {}, {}: num_trails {} score {}", .{ r, c, cur_num_trails, solutions.count() });
+                num_trails += cur_num_trails;
+                score += solutions.count();
             }
         }
     }
-    std.log.debug("Part 1 solution is {}", .{result});
+    std.log.debug("Part 1 solution is {}", .{score});
+    std.log.debug("Part 2 solution is {}", .{num_trails});
 }
 
 pub fn main() !void {
