d3cce8313a4be5e5998383b2b0f78615fd2fcb80
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-12-14 21:10:42 +0100

    Day 14 part 1

---
+++ 2024/day14/main.zig
@@ -0,0 +1,116 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const result = gpa.deinit();
+        if (result == .leak) {
+            std.log.err("Memory leaks!", .{});
+        }
+    }
+    const allocator = gpa.allocator();
+
+    const data = try std.fs.cwd().readFileAlloc(allocator, "realinput.txt", std.math.maxInt(usize));
+    defer allocator.free(data);
+    try part1(allocator, data);
+}
+
+const Location = struct {
+    x: isize,
+    y: isize,
+};
+
+const Velocity = struct {
+    dx: isize,
+    dy: isize,
+};
+
+const WIDTH: isize = 101;
+const HEIGHT: isize = 103;
+
+const Robot = struct {
+    location: Location,
+    velocity: Velocity,
+
+    fn move(self: *Robot, seconds: usize) void {
+        const times: isize = @intCast(seconds);
+
+        const x = @mod(self.location.x + self.velocity.dx * times, WIDTH);
+        // if (x < 0) {
+        //     x = WIDTH + x;
+        // }
+
+        const y = @mod(self.location.y + self.velocity.dy * times, HEIGHT);
+        // if (y < 0) {
+        //     y = HEIGHT + y;
+        // }
+        self.location.x = x;
+        self.location.y = y;
+    }
+};
+
+pub fn parseRobots(allocator: std.mem.Allocator, data: []const u8) !std.ArrayList(Robot) {
+    var robots = std.ArrayList(Robot).init(allocator);
+
+    var lineIter = std.mem.splitScalar(u8, data, '\n');
+    while (lineIter.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+        var iter = std.mem.splitScalar(u8, line, ' ');
+        const loc = iter.next().?;
+        const vel = iter.next().?;
+
+        var locIter = std.mem.splitScalar(u8, loc[2..], ',');
+        const locX = try std.fmt.parseInt(isize, locIter.next().?, 0);
+        const locY = try std.fmt.parseInt(isize, locIter.next().?, 0);
+
+        var velIter = std.mem.splitScalar(u8, vel[2..], ',');
+        const velX = try std.fmt.parseInt(isize, velIter.next().?, 0);
+        const velY = try std.fmt.parseInt(isize, velIter.next().?, 0);
+
+        try robots.append(Robot{
+            .location = Location{
+                .x = locX,
+                .y = locY,
+            },
+            .velocity = Velocity{
+                .dx = velX,
+                .dy = velY,
+            },
+        });
+    }
+    return robots;
+}
+
+pub fn part1(allocator: std.mem.Allocator, data: []const u8) !void {
+    const SECONDS: usize = 100;
+
+    var robots = try parseRobots(allocator, data);
+    defer robots.deinit();
+
+    var quads = [4]usize{ 0, 0, 0, 0 };
+
+    for (robots.items) |*robot| {
+        // std.log.debug("Initial position {},{}", .{ robot.location.x, robot.location.y });
+        robot.move(SECONDS);
+        // std.log.debug("Final position {},{}", .{ robot.location.x, robot.location.y });
+
+        if (robot.location.x == WIDTH / 2) {
+            continue;
+        }
+        const qx: usize = @intFromBool(robot.location.x > (WIDTH / 2));
+
+        if (robot.location.y == HEIGHT / 2) {
+            continue;
+        }
+        const qy: usize = @intFromBool(robot.location.y > (HEIGHT / 2));
+        const q = 2 * qy + qx;
+
+        quads[q] += 1;
+        // std.log.debug("Quad {}", .{q});
+    }
+
+    std.log.debug("Result {},{},{},{}", .{ quads[0], quads[1], quads[2], quads[3] });
+    std.log.debug("Result {}", .{quads[0] * quads[1] * quads[2] * quads[3]});
+}
