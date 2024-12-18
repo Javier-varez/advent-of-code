dff90a7b0b6f688152fafd671b3df6eb6c740a5c
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-12-19 00:28:16 +0100

    Day 18 part 2

--- 2024/day18/solution.py
+++ 2024/day18/solution.py
@@ -18,30 +18,39 @@ num_pieces = 1024
 
 start = (0, 0)
 end = (dimension, dimension)
-map = construct_map(dimension, coords[:num_pieces])
 
-pq = []
-heapq.heappush(pq, (0, start[0], start[1]))
-min_cost = {}
+def solve(num_pieces):
+    map = construct_map(dimension, coords[:num_pieces])
 
-while pq:
-    cost,x,y = heapq.heappop(pq)
-    if cost > min_cost.get((x,y), float("inf")):
-        continue
+    pq = []
+    heapq.heappush(pq, (0, start[0], start[1]))
+    min_cost = {}
 
-    min_cost[(x,y)] = cost
-    if (x,y) == end:
-        break;
-
-    for (nx,ny) in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-        if nx < 0 or ny < 0 or nx > dimension or ny > dimension:
+    while pq:
+        cost,x,y = heapq.heappop(pq)
+        if cost > min_cost.get((x,y), float("inf")):
             continue
 
-        if map[ny][nx] == '#':
-            continue
+        min_cost[(x,y)] = cost
+        if (x,y) == end:
+            break;
 
-        cur_cost = cost + 1
-        if not (cur_cost, nx, ny) in pq:
-            heapq.heappush(pq, (cur_cost, nx, ny))
+        for (nx,ny) in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+            if nx < 0 or ny < 0 or nx > dimension or ny > dimension:
+                continue
 
-print(f'{min_cost[end]}')
+            if map[ny][nx] == '#':
+                continue
+
+            cur_cost = cost + 1
+            if not (cur_cost, nx, ny) in pq:
+                heapq.heappush(pq, (cur_cost, nx, ny))
+
+    return end in min_cost
+
+i = num_pieces
+while solve(i):
+    print(f'solved {i}')
+    i += 1
+
+print(f'unsolved {i-1}: {coords[i-1]}')
