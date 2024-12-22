7ac0fe03844002d0bb2419c31cd9ca3fcc58b87f
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2024-12-23 00:48:46 +0100

    Day 22 part 2

--- 2024/day22/solution.py
+++ 2024/day22/solution.py
@@ -23,11 +23,61 @@ def next(n):
     n = prune(n)
     return n
 
-def nth(n, t):
-    for i in range(0,t):
+def generate_prices(n, num_prices):
+    prices = []
+    prices.append(n % 10)
+    for i in range(0, num_prices):
         n = next(n)
-    return n
+        prices.append(n % 10)
+    return prices
+
+def calc_delta(price_list):
+    last = price_list[0]
+    delta = []
+    for price in price_list[1:]:
+        delta.append(price - last)
+        last = price
+    return delta
 
 with open(fileName) as f:
-    result = sum([nth(int(n.strip()), 2000) for n in f.readlines()])
-    print(result)
+    initial_secrets = [int(n.strip()) for n in f.readlines()]
+
+prices = [generate_prices(seed, 2000) for seed in initial_secrets]
+price_deltas = [calc_delta(price_list) for price_list in prices]
+
+sequences = {}
+for buyer in price_deltas:
+    for i in range(0, 2000-4):
+        seq = tuple(buyer[i:i+4])
+        n = sequences.get(seq, 0)
+        sequences[seq] = n+1
+
+sequences = [(count, seq) for seq, count in sequences.items()]
+sequences.sort()
+sequences.reverse()
+
+buyer_maps = []
+for buyer_prices, buyer_deltas in zip(prices, price_deltas):
+    cur_map = {}
+    for i in range(0,2000-4):
+        seq = tuple(buyer_deltas[i:i+4])
+        if seq not in cur_map:
+            cur_map[seq] = buyer_prices[i + 4]
+    buyer_maps.append(cur_map)
+
+print(buyer_maps[0])
+current = 0
+for count, seq in sequences:
+    best_case = count * 9
+    if current > best_case:
+        break
+
+    total = 0
+    for buyer_map in buyer_maps:
+        if seq in buyer_map:
+            total += buyer_map[seq]
+
+    if total > current:
+        current = total
+
+print(f'best is {current}')
