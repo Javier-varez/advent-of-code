613cded062b79a4a788a191d865e9f1c9595c617
Javier Alvarez <javier.alvarez@allthingsembedded.net>
2025-12-08 16:16:00 +0100

    2025 Day 5 Part 2

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -2,13 +2,41 @@ fn is_fresh(id: usize, ranges: &[(usize, usize)]) -> bool {
     ranges.iter().any(|(begin, end)| id >= *begin && id <= *end)
 }
 
+fn join_ranges(left: (usize, usize), right: (usize, usize)) -> Option<(usize, usize)> {
+    if left.1 >= right.0 && right.1 >= left.0 {
+        let begin = left.0.min(right.0);
+        let end = left.1.max(right.1);
+        Some((begin, end))
+    } else {
+        None
+    }
+}
+
+fn deduplicate_ranges(ranges: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    let mut new_ranges = vec![];
+
+    for range in ranges {
+        if let Some((id, new)) = new_ranges
+            .iter_mut()
+            .enumerate()
+            .find_map(|(id, new)| join_ranges(*new, *range).map(|v| (id, v)))
+        {
+            new_ranges[id] = new;
+        } else {
+            new_ranges.push(*range);
+        }
+    }
+
+    new_ranges
+}
+
 fn main() {
     let mut args = std::env::args();
     let filename = args.nth(1).unwrap();
 
     let data = std::fs::read_to_string(filename).unwrap();
 
-    let ranges: Vec<_> = data
+    let mut ranges: Vec<_> = data
         .lines()
         .take_while(|l| !l.is_empty())
         .map(|l| {
@@ -33,6 +61,35 @@ fn main() {
     let val = available.iter().fold(0, |acc, id| {
         acc + if is_fresh(*id, &ranges) { 1 } else { 0 }
     });
-    println!("ranges: {ranges:?}, ingredient_ids: {available:?}");
     println!("count: {val}");
+
+    loop {
+        let new_ranges = deduplicate_ranges(&ranges);
+        if new_ranges.len() == ranges.len() {
+            break;
+        }
+        ranges = new_ranges;
+    }
+
+    let result = ranges
+        .iter()
+        .fold(0, |acc, (begin, end)| acc + (end - begin + 1));
+    println!("fresh_ids: {result}");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn join_ranges_test() {
+        assert_eq!(join_ranges((0, 12), (12, 23)), Some((0, 23)));
+        assert_eq!(join_ranges((0, 13), (12, 23)), Some((0, 23)));
+        assert_eq!(join_ranges((0, 24), (12, 23)), Some((0, 24)));
+        assert_eq!(join_ranges((11, 24), (12, 23)), Some((11, 24)));
+        assert_eq!(join_ranges((12, 24), (12, 23)), Some((12, 24)));
+        assert_eq!(join_ranges((13, 24), (12, 23)), Some((12, 24)));
+        assert_eq!(join_ranges((0, 11), (12, 23)), None);
+        assert_eq!(join_ranges((12, 23), (0, 11)), None);
+    }
 }
